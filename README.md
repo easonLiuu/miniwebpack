ba4b260ce5e927a1d53d081a69bff2a027971f54
liujiarui <ljr3289595084@163.com>
2023-03-31 17:44:12 +0800

    feat: 查找所有的依赖模块

--- README.md
+++ README.md
@@ -4,31 +4,29 @@
 ## 01 webpack如何工作
 
 - 如何执行打包命令，如npx webpack
-
 - 打包完输出到dist目录，通过html文件引入并在浏览器上运行
-
 - npx可以执行node_modules目录下的包
-
 - npm init -y 自动生成一个package.json依赖包管理文件
-
 - 分析打包生成的文件bundle.js的结构
-
 - 实现：执行xx命令，从webpack.config.js打包项目并生成bundle.js结构的文件
-
 - 分析bundle.js文件
-
   - 文件结构：(fun(modules){处理模块加载})(indexfile: fun, demo: fun)
-
   - 将"./demo"引入文件路径补全，转换后是"./demo.js"
-
   - 将require替换成webpack自定义加载函数`__webpack_require__`
 
 ## 02 创建打包命令 mnpack
 
 - 创建命令的目录
-
 - 生成package.json
-
 - 生成命令， npm link 把本地目录创建全局快捷方式
-
 - npx mnpack 先全局找 再本地找
+
+## 03 查找所有的依赖模块
+
+- 读取代码内容
+- 读取模块文件相对路径
+- 读取模块文件中子依赖包，首先需要解析当前模块
+- 解析结果：
+  - 是否存在子依赖包dependencies
+  - 解析的源码sourceCode
+  - 代码解析： vue->html、css、js      es6->es5
--- mnpack/lib/Compiler.js
+++ mnpack/lib/Compiler.js
@@ -1,19 +1,47 @@
 let path = require("path")
+let fs = require("fs")
 //编译器
 class Compiler {
     //config: webpack.config.js
     constructor(config) {
         this.config = config
+        this.entryId //存储入口文件路径
         this.entry = config.entry
         //当前工作目录
         this.root = process.cwd()
+        //用来保存所有模块依赖
+        this.modules = {}
+    }
+    //读取模块内容
+    getSource(modulePath) {
+        return fs.readFileSync(modulePath, "utf-8")
+    }
+    //模块文件解析 source--文件内容 parentPath--文件目录
+    //解析结果 - 是否存在子依赖包dependencies 解析的源码sourceCode
+    parse(source, parentPath) {
+
     }
     //从root结点找所有的依赖模块
     //modulePath模块文件路径 isEntry是否是入口文件
     buildModule(modulePath, isEntry) {
-        console.log(modulePath)
+        // console.log(modulePath) /Users/liujiarui/Desktop/miniwebpack/src/index.js
+        let source = this.getSource(modulePath)
+        // 拿到相对路径
+        let moduleName = "./" + path.relative(this.root, modulePath)
+        // console.log(moduleName) ./src/index.js
+        if (isEntry) {
+            this.entryId = moduleName //存储入口文件路径
+        }
+        //解析结果 - 是否存在子依赖包dependencies 解析的源码sourceCode
+        let { sourceCode, dependencies } = this.parse(source, path.dirname(moduleName))
+        // console.log(path.dirname(moduleName))   ./src
+        // 保存解析的代码
+        this.modules[moduleName] = sourceCode
+        //递归
+        dependencies.forEach(dep => {
+            this.buildModule(path.join(this.root, dep), false)
+        })
     }
-
     //执行方法 用于编译
     run() {
         this.buildModule(path.resolve(this.root, this.entry), true)
